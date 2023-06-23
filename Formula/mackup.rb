f4b5eecd43aff15ae9e7c5f9e4588b32f643e76b
James Roberts-Thomson <jamesrt@gmail.com>
2023-06-23 16:09:46 +1200

    Intial commit

---
+++ Formula/mackup.rb
@@ -0,0 +1,31 @@
+class Mackup < Formula
+  include Language::Python::Virtualenv
+
+  desc "Keep your Mac's application settings in sync"
+  homepage "https://github.com/jamesrtnz/mackup"
+  url "file:///Users/james/GIT/JRT/jrt-mackup/dist/mackup-0.8.37a0.tar.gz"
+  sha256 "d0f8ae49c754d5fff1b416a6f5fbd91d6b23e136cc63bb186af0197de0da1f60"
+  license "GPL-3.0-or-later"
+  head "https://github.com/jamesrtnz/mackup.git", branch: "master"
+
+  bottle do
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d0f8ae49c754d5fff1b416a6f5fbd91d6b23e136cc63bb186af0197de0da1f60"
+  end
+
+  depends_on "python@3.11"
+  depends_on "six"
+
+  resource "docopt" do
+    url "https://files.pythonhosted.org/packages/a2/55/8f8cab2afd404cf578136ef2cc5dfb50baa1761b68c9da1fb1e4eed343c9/docopt-0.6.2.tar.gz"
+    sha256 "49b3a825280bd66b3aa83585ef59c4a8c82f2c8a522dbe754a8bc8d08c85c491"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    system "#{bin}/mackup", "--help"
+  end
+end
---
+++ README.md
@@ -0,0 +1,13 @@
+# mackup Homebrew tap
+
+The main purpose of this repo is to provide a tap for a formula for my fork of [mackup](https://github.com/jamesrtnz/mackup/)
+
+## How do I install these formulae?
+
+`brew install jamesrtnz/mackup/mackup`
+
+Or `brew tap jamesrtnz/mackup` and then `brew install mackup`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
